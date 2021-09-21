29983e78e94eea2d86166ce3a18990b6d42e7408
Nikola Hristov <nikola@playform.cloud>
2021-09-21 21:40:24 +0300

    Initial commit

---
+++ src/assets/js/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCqdOxv8WlI90ZeLZHjPOXtm0BWib935gM",
+	appId: "1:686609704824:web:f90dcd21825de0d2744a8d",
+	authDomain: "souqrs.firebaseapp.com",
+	databaseURL: "https://souqrs-default-rtdb.firebaseio.com",
+	measurementId: "G-8719G8T6RB",
+	messagingSenderId: "686609704824",
+	projectId: "souqrs",
+	storageBucket: "souqrs.appspot.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const performance = getPerformance(app);
