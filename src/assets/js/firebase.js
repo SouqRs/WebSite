686a9a9a75029fac9dd77411add579fa92b3669f
Nikola Hristov <nikola@playform.cloud>
2021-10-13 23:37:20 +0300

    Switches realtime Firebase database

--- src/assets/js/firebase.js
+++ src/assets/js/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
 	apiKey: "AIzaSyCqdOxv8WlI90ZeLZHjPOXtm0BWib935gM",
 	appId: "1:686609704824:web:f90dcd21825de0d2744a8d",
 	authDomain: "souqrs.firebaseapp.com",
-	databaseURL: "https://souqrs.firebaseio.com",
+	databaseURL: "https://souqrs-default-rtdb.firebaseio.com/",
 	measurementId: "G-8719G8T6RB",
 	messagingSenderId: "686609704824",
 	projectId: "souqrs",
