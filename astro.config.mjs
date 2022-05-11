c9711cf67bdb72c95df8fcc97da8768dcb5b893b
Nikola Hristov <nikola@playform.cloud>
2022-05-12 02:59:57 +0300

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import compress from "astro-compress";
 
 export default defineConfig({
 	site: "https://souq.dev",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
