1bc9ec30de5efdeb5bb73c2e0699409d89bac385
Nikola Hristov <nikola@playform.cloud>
2022-05-28 16:42:38 +0300

    Clear dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,7 @@ import critters from "astro-critters";
 import compress from "astro-compress";
 
 export default defineConfig({
-<<<<<<< HEAD
 	site: "https://souq.dev",
-=======
-	// TODO Place your site URL here
-	// site: "",
->>>>>>> upstream/main
 	experimental: {
 		integrations: true,
 	},
