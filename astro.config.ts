4d88cc00fb481a990e0b80e0a0c2fbf035e93286
Nikola Hristov <nikola@playform.cloud>
2022-08-15 06:07:53 +0300

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,11 @@ import critters from "astro-critters";
 export default /** @type {import('astro').AstroUserConfig} */ {
 	site: "https://souq.dev",
 
-	integrations: [sitemap(), critters(), compress()],
+	integrations: [
+		sitemap(),
+		critters(),
+		compress({
+			logger: 1,
+		}),
+	],
 };
