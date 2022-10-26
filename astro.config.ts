bff94c2d709e94825feb7d2932cd789830554828
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-27 01:18:21 +0300

    squash!

---
+++ astro.config.ts
@@ -0,0 +1,15 @@
+import compress from "astro-compress";
+import critters from "astro-critters";
+
+import sitemap from "@astrojs/sitemap";
+
+export default {
+	site: "https://souq.dev",
+	integrations: [
+		sitemap(),
+		critters(),
+		compress({
+			logger: 1,
+		}),
+	],
+};
---
+++ src/scripts/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCqdOxv8WlI90ZeLZHjPOXtm0BWib935gM",
+	appId: "1:686609704824:web:f90dcd21825de0d2744a8d",
+	authDomain: "souqrs.firebaseapp.com",
+	databaseURL: "https://souqrs-default-rtdb.firebaseio.com/",
+	measurementId: "G-8719G8T6RB",
+	messagingSenderId: "686609704824",
+	projectId: "souqrs",
+	storageBucket: "souqrs.appspot.com",
+};
+
+initializeApp(firebaseConfig);
