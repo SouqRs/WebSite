106c766a6164812902ba9e5af1402a68b7ff98be
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-22 05:06:23 +0200

    squash!

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCqdOxv8WlI90ZeLZHjPOXtm0BWib935gM",
-	appId: "1:686609704824:web:f90dcd21825de0d2744a8d",
 	authDomain: "souqrs.firebaseapp.com",
-	databaseURL: "https://souqrs-default-rtdb.firebaseio.com/",
-	measurementId: "G-8719G8T6RB",
-	messagingSenderId: "686609704824",
+	databaseURL: "https://souqrs-default-rtdb.firebaseio.com",
 	projectId: "souqrs",
 	storageBucket: "souqrs.appspot.com",
+	messagingSenderId: "686609704824",
+	appId: "1:686609704824:web:f90dcd21825de0d2744a8d",
+	measurementId: "G-MEVM3LGWJH"
 };
 
 initializeApp(firebaseConfig);
