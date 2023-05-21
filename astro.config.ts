8d9ecbc419437279bc8fdf0049079a2427dcdc13
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-21 17:58:55 +0300

    squash!

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,9 @@ import worker from "astrojs-service-worker";
 
 export default defineConfig({
 	site: "https://souq.dev",
+	experimental: {
+		assets: true,
+	},
 	integrations: [
 		import.meta.env.MODE === "production" ? worker() : null,
 		sitemap(),
